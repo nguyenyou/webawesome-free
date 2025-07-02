5fdbbfa8b75fb7628f18e0d1f9a76fa7953ba231
nguyenyou <tunt081295@gmail.com>
2025-07-03 00:45:41 +0700

    🚢 ship it

--- src/components/badge/Badge.stories.ts
+++ src/components/badge/Badge.stories.ts
@@ -1,12 +1,51 @@
 import "./badge.js";
 import { html } from "lit";
+import type { Meta, StoryObj } from "@storybook/web-components-vite";
+import { ifDefined } from 'lit/directives/if-defined.js';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
-const meta = {
-  title: "Example/Badge",
+
+type BadgeProps = {
+  variant: "brand" | "neutral" | "success" | "warning" | "danger";
+  appearance: "accent" | "filled" | "outlined";
+  attention: "pulse" | "bounce" | "none";
+  pill: boolean;
 };
 
+const meta: Meta<BadgeProps> = {
+  component: "wa-badge",
+  title: "Example/Badge",
+  argTypes: {
+    variant: {
+      control: "select",
+      options: ["brand", "neutral", "success", "warning", "danger"],
+    },
+    appearance: {
+      control: "select",
+      options: ["accent", "filled", "outlined"],
+    },
+    attention: { control: "select", options: ["pulse", "bounce", "none"] },
+    pill: { control: "boolean" },
+  },
+  render: (args) => {
+    return html`
+      <wa-badge variant="${ifDefined(args.variant)}" appearance="${ifDefined(args.appearance)}" attention="${ifDefined(args.attention)}" ?pill="${args.pill}">Badge</wa-badge>
+    `;
+  },
+} satisfies Meta<BadgeProps>;
+
 export default meta;
+
+type Story = StoryObj<BadgeProps>;
+
+export const Primary: Story = {
+  args: {
+    variant: "brand",
+    appearance: "accent",
+    attention: "none",
+    pill: false,
+  },
+};
 
 export const Default = () => {
   return html`
@@ -78,5 +117,65 @@ export const Appearances = () => {
       <wa-badge appearance="filled" variant="danger">Filled</wa-badge>
       <wa-badge appearance="outlined" variant="danger">Outlined</wa-badge>
     </div>
+  `;
+};
+
+export const Sizes = () => {
+  return html`
+    <wa-badge variant="brand" style="font-size: var(--wa-font-size-xs);"
+      >Brand</wa-badge
+    >
+    <wa-badge variant="brand" style="font-size: var(--wa-font-size-s);"
+      >Brand</wa-badge
+    >
+    <wa-badge variant="brand" style="font-size: var(--wa-font-size-m);"
+      >Brand</wa-badge
+    >
+    <wa-badge variant="brand" style="font-size: var(--wa-font-size-l);"
+      >Brand</wa-badge
+    >
+    <wa-badge variant="brand" style="font-size: var(--wa-font-size-xl);"
+      >Brand</wa-badge
+    >
+  `;
+};
+
+export const Pill = () => {
+  return html`
+    <wa-badge variant="brand" pill>Brand</wa-badge>
+    <wa-badge variant="success" pill>Success</wa-badge>
+    <wa-badge variant="neutral" pill>Neutral</wa-badge>
+    <wa-badge variant="warning" pill>Warning</wa-badge>
+    <wa-badge variant="danger" pill>Danger</wa-badge>
+  `;
+};
+
+export const DrawingAttention = () => {
+  return html`
+    <div class="badge-attention">
+      <wa-badge variant="brand" attention="pulse" pill>1</wa-badge>
+      <wa-badge variant="success" attention="pulse" pill>1</wa-badge>
+      <wa-badge variant="neutral" attention="pulse" pill>1</wa-badge>
+      <wa-badge variant="warning" attention="pulse" pill>1</wa-badge>
+      <wa-badge variant="danger" attention="pulse" pill>1</wa-badge>
+    </div>
+
+    <div class="badge-attention">
+      <wa-badge variant="brand" attention="bounce" pill>1</wa-badge>
+      <wa-badge variant="success" attention="bounce" pill>1</wa-badge>
+      <wa-badge variant="neutral" attention="bounce" pill>1</wa-badge>
+      <wa-badge variant="warning" attention="bounce" pill>1</wa-badge>
+      <wa-badge variant="danger" attention="bounce" pill>1</wa-badge>
+    </div>
+
+    <style>
+      .badge-attention {
+        margin-block-end: var(--wa-space-m);
+
+        wa-badge:not(:last-of-type) {
+          margin-right: 1rem;
+        }
+      }
+    </style>
   `;
 };
