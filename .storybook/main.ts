8f1d7ad94839a87d19cecdd4ffeb8eb4f81e2ce6
nguyenyou <tunt081295@gmail.com>
2025-07-28 22:05:04 +0700

    🚢 ship it

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,8 @@
+import { createRequire } from "node:module";
+import { dirname, join } from "node:path";
 import type { StorybookConfig } from '@storybook/web-components-vite';
+
+const require = createRequire(import.meta.url);
 
 const config: StorybookConfig = {
   "stories": [
@@ -6,10 +10,10 @@ const config: StorybookConfig = {
     "../docs/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
+    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/addon-docs"),
+    getAbsolutePath("@storybook/addon-a11y"),
+    getAbsolutePath("@storybook/addon-vitest")
   ],
   "framework": {
     "name": "@storybook/web-components-vite",
@@ -21,3 +25,7 @@ const config: StorybookConfig = {
   }
 };
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
